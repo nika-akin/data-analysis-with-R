f6fad1d2ea7ed5e5a7360be8192a08c62486011d
nika-akin <batzdorfer.veronika@gmail.com>
2025-05-20 17:47:54 +0200

    :zap: first session

--- content/R/course_content.R
+++ content/R/course_content.R
@@ -1,95 +1,89 @@
-course_content_1 <-
-  tibble::tribble(
-    ~Day, ~Time, ~Topic,
-    "Wednesday", "12:00 - 13:00", "Onboarding & Getting Started with R",
-    "Wednesday", "13:00 - 13:15", "Break",
-    "Wednesday", "13:15 - 14:30", "Data Types & Loading",
-    "Wednesday", "14:30 - 14:45", "Break",
-    "Wednesday", "14:45 - 16:00", "Data Workflows & Wrangling",
-    "Wednesday", "16:00 - 16:45", "Open Trouble Shooting Session",
+# Day 1: 23-05
+course_day1 <- tibble::tribble(
+  ~Day, ~Time, ~Topic,
+  "Friday", "12:00 - 13:00", "Onboarding & Getting Started with R",
+  "Friday", "13:00 - 13:15", "Break",
+  "Friday", "13:15 - 14:00", "Data Types & Loading",
+  "Friday", "14:00 - 15:00", "Lunch Break",
+  "Friday", "15:00 - 16:00", "Data Wrangling",
+  "Friday", "16:00 - 16:15", "Break",
+  "Friday", "16:15 - 17:00", "Data Workflows & Github"
+) %>%
+  knitr::kable(escape = FALSE) %>%
+  kableExtra::kable_styling() %>%
+  kableExtra::column_spec(1, color = "darkgreen") %>%
+  kableExtra::column_spec(2, color = "darkgreen") %>%
+  kableExtra::column_spec(3, bold = TRUE) %>%
+  kableExtra::row_spec(c(2, 4, 6), color = "gray")
 
-  ) %>%
+# Day 2: 24-05
+course_day2 <- tibble::tribble(
+  ~Day, ~Time, ~Topic,
+  "Saturday", "12:00 - 13:00", "Data Wrangling 2.0",
+  "Saturday", "13:00 - 13:15", "Break",
+  "Saturday", "13:15 - 14:00", "Exploratory Analyses",
+  "Saturday", "14:00 - 15:00", "Lunch Break",
+  "Saturday", "15:00 - 16:00", "Data Visualization",
+  "Saturday", "16:00 - 16:15", "Break",
+  "Saturday", "16:15 - 17:00", "Relational Data"
+) %>%
   knitr::kable() %>%
   kableExtra::kable_styling() %>%
   kableExtra::column_spec(1, color = "darkgreen") %>%
   kableExtra::column_spec(2, color = "darkgreen") %>%
   kableExtra::column_spec(3, bold = TRUE) %>%
-  kableExtra::row_spec(2, color = "gray") %>%
-  kableExtra::row_spec(4, color = "gray") %>%
-  kableExtra::row_spec(6, color = "gray")
+  kableExtra::row_spec(c(2, 4, 6), color = "gray")
 
-course_content_2 <-
-  tibble::tribble(
-    ~Day, ~Time, ~Topic,
-    "Thursday", "12:00 - 13:00", "Data Wrangling",
-    "Thursday", "13:00 - 13:15", "Break",
-    "Thursday", "13:15 - 14:30", "Exploratory Data Analysis",
-    "Thursday", "14:30 - 14:45", "Break",
-    "Thursday", "14:45 - 16:00", "Relational Data",
-    "Thursday", "16:00 - 16:45", "Open Trouble Shooting Session",
-
-  ) %>%
+# Day 3: 23-06
+course_day3 <- tibble::tribble(
+  ~Day, ~Time, ~Topic,
+  "Monday", "12:00 - 13:00", "Recap",
+  "Monday", "13:00 - 13:15", "Break",
+  "Monday", "13:15 - 14:00", "Confirmatory Analyses",
+  "Monday", "14:00 - 15:00", "Lunch Break",
+  "Monday", "15:00 - 16:00", "Reproducible Reporting with R Markdown",
+  "Monday", "16:00 - 16:15", "Break",
+  "Monday", "16:15 - 17:00", "R Markdown revisited"
+) %>%
   knitr::kable() %>%
   kableExtra::kable_styling() %>%
   kableExtra::column_spec(1, color = "darkgreen") %>%
   kableExtra::column_spec(2, color = "darkgreen") %>%
   kableExtra::column_spec(3, bold = TRUE) %>%
-  kableExtra::row_spec(2, color = "gray") %>%
-  kableExtra::row_spec(4, color = "gray") %>%
-  kableExtra::row_spec(6, color = "gray")
+  kableExtra::row_spec(c(2, 4, 6), color = "gray")
 
-course_content_3 <-
-  tibble::tribble(
-    ~Day, ~Time, ~Topic,
-    "Friday", "12:00 - 13:00", "Visualization & Exploratory Data Analysis",
-    "Friday", "13:00 - 13:15", "Break",
-    "Friday", "13:15 - 14:30", "Reporting with R Markdown",
-    "Friday", "14:30 - 14:45", "Break",
-    "Friday", "14:45 - 16:00", "Wrap-up (Evaluation) & Group Session",
-    "Friday", "16:00 - 16:45", "Open Trouble Shooting Session",
-  ) %>%
+# Day 4: 24-06
+course_day4 <- tibble::tribble(
+  ~Day, ~Time, ~Topic,
+  "Tuesday", "12:00 - 13:00", "Run LLMs locally",
+  "Tuesday", "13:00 - 13:15", "Break",
+  "Tuesday", "13:15 - 14:00", "LLMs in RStudio",
+  "Tuesday", "14:00 - 15:00", "Lunch Break",
+  "Tuesday", "15:00 - 16:00", "Group data exploration",
+  "Tuesday", "16:00 - 16:15", "Break",
+  "Tuesday", "16:15 - 17:00", "Group data exploration"
+) %>%
   knitr::kable() %>%
   kableExtra::kable_styling() %>%
   kableExtra::column_spec(1, color = "darkgreen") %>%
   kableExtra::column_spec(2, color = "darkgreen") %>%
   kableExtra::column_spec(3, bold = TRUE) %>%
-  kableExtra::row_spec(2, color = "gray") %>%
-  kableExtra::row_spec(4, color = "gray") %>%
-  kableExtra::row_spec(6, color = "gray")
+  kableExtra::row_spec(c(2, 4, 6), color = "gray")
 
-course_content_4 <-
-  tibble::tribble(
-    ~Day, ~Time, ~Topic,
-    "Thursday", "09:30 - 10:30", "Confirmatory Data Analysis",
-    "Thursday", "10:30 - 10:45", "Break",
-    "Thursday", "10:45 - 12:00", "Confirmatory Data Analysis",
-    "Thursday", "12:00 - 13:00", "Lunch Break",
-    "Thursday", "13:00 - 14:00", "Data Visualization - Part 2",
-    "Thursday", "14:00 - 14:15", "Break",
-    "Thursday", "14:15 - 15:30", "Data Visualization - Part 2"
-  ) %>%
+# Day 5: 25-06
+course_day5 <- tibble::tribble(
+  ~Day, ~Time, ~Topic,
+  "Wednesday", "12:00 - 13:00", "Group data challenge",
+  "Wednesday", "13:00 - 13:15", "Break",
+  "Wednesday", "13:15 - 14:00", "Group data challenge",
+  "Wednesday", "14:00 - 15:00", "Lunch Break",
+  "Wednesday", "15:00 - 16:00", "Group presentations",
+  "Wednesday", "16:00 - 16:15", "Break",
+  "Wednesday", "16:15 - 17:00", "Wrap-up"
+) %>%
   knitr::kable() %>%
   kableExtra::kable_styling() %>%
-  kableExtra::column_spec(1, color = "gray") %>%
-  kableExtra::column_spec(2, color = "gray") %>%
+  kableExtra::column_spec(1, color = "darkgreen") %>%
+  kableExtra::column_spec(2, color = "darkgreen") %>%
   kableExtra::column_spec(3, bold = TRUE) %>%
-  kableExtra::row_spec(2, color = "gray") %>%
-  kableExtra::row_spec(4, color = "gray") %>%
-  kableExtra::row_spec(6, color = "gray")
-
-course_content_5 <-
-  tibble::tribble(
-    ~Day, ~Time, ~Topic,
-    "Friday", "09:30 - 10:30", "Reporting with R Markdown",
-    "Friday", "10:30 - 10:45", "Break",
-    "Friday", "10:45 - 12:30", "Reporting with R Markdown",
-    "Friday", "12:30 - 13:30", "Lunch Break",
-    "Friday", "13:45 - 14:30", "Outlook, Q&A",
-  ) %>%
-  knitr::kable() %>%
-  kableExtra::kable_styling() %>%
-  kableExtra::column_spec(1, color = "gray") %>%
-  kableExtra::column_spec(2, color = "gray") %>%
-  kableExtra::column_spec(3, bold = TRUE) %>%
-  kableExtra::row_spec(2, color = "gray") %>%
-  kableExtra::row_spec(4, color = "gray")
+  kableExtra::row_spec(c(2, 4, 6), color = "gray")
